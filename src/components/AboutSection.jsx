df149ff64dc30c89794e13ce094221a1f1e8962d
Logan <loganheath90@gmail.com>
2025-08-26 20:44:38 -0700

    theme appearance change

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/resume.docx updated.pdf"
+                href="/resume.pdf"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
